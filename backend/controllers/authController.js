3f4f5a0ecd755f60d2b2d754ced4b97d8d0484bd
vinuthna <enjoycoding2811@gmail.com>
2026-05-30 22:55:02 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -136,14 +136,21 @@ export const googleCallback = async (req, res) => {
     }
 
     const token = jwt.sign(
-      { id: req.user._id },
-      process.env.JWT_SECRET,
-      { expiresIn: "7d" }
-    );
+  { id: req.user._id },
+  process.env.JWT_SECRET,
+  { expiresIn: "7d" }
+);
 
-    res.redirect(
-      `${process.env.FRONTEND_URL}/oauth-success?token=${token}`
-    );
+res.cookie("token", token, {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: "lax",
+  maxAge: 7 * 24 * 60 * 60 * 1000
+});
+
+res.redirect(
+  `${process.env.FRONTEND_URL}/oauth-success`
+);
 
   } catch (error) {
 
